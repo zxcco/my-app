154d734dcf57caa3de73d5233c76d30826ad4a9e
netiphong <netiphong234567@gmail.com>
2023-08-04 14:34:03 +0700

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+ 
+  myString = "Hello, world!";
   title = 'my-app';
+  constructor(private http: HttpClient) {}
+
+  click() {
+    const apiUrl = `http://localhost:9000/technician-api/dropdown/test=${this.myString}`;
+    const queryParam = encodeURIComponent(this.myString);
+    const apiURLWithQueryParam = `${apiUrl}?text=${queryParam}`;
+
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        // Handle the successful response from the API here
+        console.log('API response:', response);
+      },
+      error: (error: any) => {
+        // Handle any errors that occurred during the API call here
+        console.error('API error:', error);
+      }
+    })
+  }
+
+
+
+
 }
+
